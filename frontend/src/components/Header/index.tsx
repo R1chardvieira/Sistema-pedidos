9987c5df8a914f53e5e90ebe3515a755557d597c
R1chardvieira <luckasrichard22@gmail.com>
2024-06-10 10:25:21 -0300

    primeiro commit

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+
+import { Container, Content} from "./style";
+
+export function Header(){
+    return(
+        <Container>
+            <Content>
+            <div className="detalhe-pagina">
+                <h1>Pedidos</h1>
+            </div>
+        
+            </Content>
+        </Container>
+    );
+}
---
+++ frontend/src/components/Header/style.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Container = styled.header`
+    background: #fea509;
+    display: flex;
+    justify-content: center;
+    height: 100px;
+    align-items: center;
+`;
+
+export const Content = styled.div`
+    width: 100%;
+    max-width: 1216px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`;
